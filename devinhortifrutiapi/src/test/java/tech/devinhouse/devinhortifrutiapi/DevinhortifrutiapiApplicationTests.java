49ff1cc80a6233b228476da3c82fd9f5d674c007
Lucas Bueno Cesario <lbc92@hotmail.com>
2022-05-23 22:11:00 -0300

    Inicialização do projeto

---
+++ devinhortifrutiapi/src/test/java/tech/devinhouse/devinhortifrutiapi/DevinhortifrutiapiApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.devinhouse.devinhortifrutiapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevinhortifrutiapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
