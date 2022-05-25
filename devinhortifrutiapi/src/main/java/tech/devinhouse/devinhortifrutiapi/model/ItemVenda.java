dbedabf456981fd92243073c6612cfb565b598aa
Lucas Bueno Cesario <lbc92@hotmail.com>
2022-05-25 15:00:09 -0300

    Adiciona getters e setters na entidade ItemVenda

--- devinhortifrutiapi/src/main/java/tech/devinhouse/devinhortifrutiapi/model/ItemVenda.java
+++ devinhortifrutiapi/src/main/java/tech/devinhouse/devinhortifrutiapi/model/ItemVenda.java
@@ -11,8 +11,8 @@ public class ItemVenda {
     @SequenceGenerator(name = "item_venda_ger", sequenceName = "item_venda_id_seq")
     private Long id;
 
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "id_venda", referencedColumnName = "id")
+    @ManyToOne(fetch = FetchType.LAZY) //deixar como LAZY ou EAGER?
+    @JoinColumn(name = "id_venda", referencedColumnName = "id")
     private Venda venda;
 
 //    @OneToOne(fetch = FetchType.EAGER)
@@ -23,4 +23,44 @@ public class ItemVenda {
     private BigDecimal precoUnitario;
 
     private Integer quantidade;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Venda getVenda() {
+        return venda;
+    }
+
+    public void setVenda(Venda venda) {
+        this.venda = venda;
+    }
+
+    public Long getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Long produto) {
+        this.produto = produto;
+    }
+
+    public BigDecimal getPrecoUnitario() {
+        return precoUnitario;
+    }
+
+    public void setPrecoUnitario(BigDecimal precoUnitario) {
+        this.precoUnitario = precoUnitario;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
 }
