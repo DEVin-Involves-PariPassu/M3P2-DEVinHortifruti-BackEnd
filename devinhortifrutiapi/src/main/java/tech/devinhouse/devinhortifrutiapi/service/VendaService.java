c48ea33cc3557f1e5f85976cf00f01735c8c6c9d
Daiana Michels <daianaem.12@gmail.com>
2022-05-25 11:53:44 -0300

    feat: inicializa a classe VendaService

--- devinhortifrutiapi/src/main/java/tech/devinhouse/devinhortifrutiapi/service/VendaService.java
+++ devinhortifrutiapi/src/main/java/tech/devinhouse/devinhortifrutiapi/service/VendaService.java
@@ -1,2 +1,17 @@
-package tech.devinhouse.devinhortifrutiapi.service;public class VendaService {
+package tech.devinhouse.devinhortifrutiapi.service;
+
+import tech.devinhouse.devinhortifrutiapi.repository.VendaRepository;
+import tech.devinhouse.devinhortifrutiapi.repository.ItemVendaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class VendaService {
+    @Autowired
+    VendaRepository vendaRepository;
+
+    @Autowired
+    ItemVendaRepository itemVendaRepository;
+
+
 }
