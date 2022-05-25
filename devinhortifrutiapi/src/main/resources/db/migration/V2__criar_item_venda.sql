f6b97561edc70fd07521c676c2fda26fb27f73ab
Lucas Bueno Cesario <lbc92@hotmail.com>
2022-05-25 15:12:43 -0300

    Corrige tabela de item_venda

---
+++ devinhortifrutiapi/src/main/resources/db/migration/V2__criar_item_venda.sql
@@ -0,0 +1,9 @@
+CREATE TABLE item_venda (
+    id bigserial primary key,
+    id_venda bigint,
+    id_produto bigint,
+    preco_unitario decimal,
+    quantidade int
+);
+
+ALTER TABLE item_venda ADD FOREIGN KEY (id_venda) REFERENCES vendas(id);
