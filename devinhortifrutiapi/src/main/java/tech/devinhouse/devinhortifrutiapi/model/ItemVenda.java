c7e446606d498e41f4941e728231f5e6157b943c
Lucas Bueno Cesario <lbc92@hotmail.com>
2022-05-25 15:07:25 -0300

    Adiciona entidade ItemVenda

---
+++ devinhortifrutiapi/src/main/java/tech/devinhouse/devinhortifrutiapi/model/ItemVenda.java
@@ -0,0 +1,65 @@
+package tech.devinhouse.devinhortifrutiapi.model;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity(name = "item_venda")
+public class ItemVenda {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "itemvendager")
+    @SequenceGenerator(name = "itemvendager", sequenceName = "item_venda_id_seq", allocationSize = 1)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_venda", referencedColumnName = "id")
+    private Venda venda;
+
+//    @OneToOne(fetch = FetchType.EAGER)
+//    @JoinColumn(name = "id_produto", referencedColumnName = "id")
+    private Long produto;
+
+    private BigDecimal precoUnitario;
+
+    private Integer quantidade;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Venda getVenda() {
+        return venda;
+    }
+
+    public void setVenda(Venda venda) {
+        this.venda = venda;
+    }
+
+    public Long getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Long produto) {
+        this.produto = produto;
+    }
+
+    public BigDecimal getPrecoUnitario() {
+        return precoUnitario;
+    }
+
+    public void setPrecoUnitario(BigDecimal precoUnitario) {
+        this.precoUnitario = precoUnitario;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+}
