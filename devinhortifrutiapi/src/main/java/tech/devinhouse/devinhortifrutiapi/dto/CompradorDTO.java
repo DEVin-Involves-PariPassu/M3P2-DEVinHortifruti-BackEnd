b349655c5837660733b1cf23c0373c01de011d1e
felixcolombo <felix_colombo@hotmail.com>
2022-05-26 19:51:46 -0300

    feat: cria atributos e metodos getter e setter Classe CompradorDTO

--- devinhortifrutiapi/src/main/java/tech/devinhouse/devinhortifrutiapi/dto/CompradorDTO.java
+++ devinhortifrutiapi/src/main/java/tech/devinhouse/devinhortifrutiapi/dto/CompradorDTO.java
@@ -1,5 +1,59 @@
 package tech.devinhouse.devinhortifrutiapi.dto;
 
-public class CompradorDTO {
+import com.sun.istack.NotNull;
 
+public class CompradorDTO {
+    private Long id;
+
+    @NotNull
+    private String nome;
+
+    @NotNull
+    private String cpf;
+
+    @NotNull
+    private String email;
+
+    @NotNull
+    private String telefone;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
 }
