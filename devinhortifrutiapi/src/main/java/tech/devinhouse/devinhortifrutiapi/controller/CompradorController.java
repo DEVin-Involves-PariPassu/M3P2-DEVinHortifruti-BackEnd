c11118716b550818765a23d9dc7260f0854059b5
felixcolombo <felix_colombo@hotmail.com>
2022-05-26 20:46:31 -0300

    feat: cria endpoint Post Cliente em CompradorController

--- devinhortifrutiapi/src/main/java/tech/devinhouse/devinhortifrutiapi/controller/CompradorController.java
+++ devinhortifrutiapi/src/main/java/tech/devinhouse/devinhortifrutiapi/controller/CompradorController.java
@@ -1,9 +1,13 @@
 package tech.devinhouse.devinhortifrutiapi.controller;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import tech.devinhouse.devinhortifrutiapi.dto.CompradorDTO;
 import tech.devinhouse.devinhortifrutiapi.service.CompradorService;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/comprador")
@@ -11,5 +15,19 @@ public class CompradorController {
 
     @Autowired
     CompradorService compradorService;
+
+    @PostMapping
+    public ResponseEntity<Long> post(
+            @RequestHeader("Authorization") String auth,
+            @RequestBody CompradorDTO comprador
+    ) {
+        Long id = compradorService.salvar(comprador);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(id).toUri();
+
+        return ResponseEntity.created(location).body(id);
+    }
 
 }
