6bf121e7557b0cc465a0d3ad307df53e0410f6a9
Victor Ferrando <victor.ferrando@hotmail.com>
2022-05-26 20:40:03 -0300

    feat: define atributos e criar metodos getters e setters no ProdutoDTO

--- devinhortifrutiapi/src/main/java/tech/devinhouse/devinhortifrutiapi/dto/ProdutoDTO.java
+++ devinhortifrutiapi/src/main/java/tech/devinhouse/devinhortifrutiapi/dto/ProdutoDTO.java
@@ -1,5 +1,74 @@
 package tech.devinhouse.devinhortifrutiapi.dto;
 
+import com.sun.istack.NotNull;
+
+import java.math.BigDecimal;
+
 public class ProdutoDTO {
+
+    private Long id;
+
+    @NotNull
+    private String nome;
+
+    @NotNull
+    private String descricao;
+
+    @NotNull
+    private String urlFoto;
+
+    @NotNull
+    private BigDecimal precoAtual;
+
+    @NotNull
+    private Boolean isActive;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getUrlFoto() {
+        return urlFoto;
+    }
+
+    public void setUrlFoto(String urlFoto) {
+        this.urlFoto = urlFoto;
+    }
+
+    public BigDecimal getPrecoAtual() {
+        return precoAtual;
+    }
+
+    public void setPrecoAtual(BigDecimal precoAtual) {
+        this.precoAtual = precoAtual;
+    }
+
+    public Boolean getActive() {
+        return isActive;
+    }
+
+    public void setActive(Boolean active) {
+        isActive = active;
+    }
 
 }
