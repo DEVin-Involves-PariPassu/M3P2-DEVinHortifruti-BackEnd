24a687a6fab2607b623e17ee8520406e0e917e27
Lufalot <lucas.f.lotaif@gmail.com>
2022-05-25 22:01:45 -0300

    feat: cria entidade Produtos

---
+++ devinhortifrutiapi/src/main/java/tech/devinhouse/devinhortifrutiapi/model/Produtos.java
@@ -0,0 +1,88 @@
+package tech.devinhouse.devinhortifrutiapi.model;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity(name = "produtos")
+public class Produtos {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "produtosger")
+  @SequenceGenerator(name = "produtosger", sequenceName = "produtos_id_seq", allocationSize = 1)
+  private Long id;
+
+  private String nome;
+
+  private String descricao;
+
+  @Column(name="url_foto")
+  private String urlFoto;
+
+  @Column(name="preco_sugerido")
+  private BigDecimal precoSugerido;
+
+  @Column(name="is_ativo")
+  private boolean isAtivo;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return nome;
+  }
+
+  public void setName(String name) {
+    this.nome = name;
+  }
+
+  public String getDescricao() {
+    return descricao;
+  }
+
+  public void setDescricao(String descricao) {
+    this.descricao = descricao;
+  }
+
+  public String getUrlFoto() {
+    return urlFoto;
+  }
+
+  public void setUrlFoto(String urlFoto) {
+    this.urlFoto = urlFoto;
+  }
+
+  public BigDecimal getPrecoSugerido() {
+    return precoSugerido;
+  }
+
+  public void setPrecoSugerido(BigDecimal precoSugerido) {
+    this.precoSugerido = precoSugerido;
+  }
+
+  public boolean isAtivo() {
+    return isAtivo;
+  }
+
+  public void setAtivo(boolean ativo) {
+    isAtivo = ativo;
+  }
+
+  public Produtos() {
+
+  }
+
+  public Produtos(Long id, String nome, String descricao, String urlFoto, BigDecimal precoSugerido, boolean isAtivo) {
+    this.id = id;
+    this.nome = nome;
+    this.descricao = descricao;
+    this.urlFoto = urlFoto;
+    this.precoSugerido = precoSugerido;
+    this.isAtivo = isAtivo;
+  }
+
+}
