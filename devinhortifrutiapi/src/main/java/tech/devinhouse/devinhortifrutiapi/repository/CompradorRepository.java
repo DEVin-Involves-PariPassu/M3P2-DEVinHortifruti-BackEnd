77d8472f1c155a6552a3ac367c8bd65a66fa6bd5
felixcolombo <felix_colombo@hotmail.com>
2022-05-26 20:47:00 -0300

    feat: cria busca por CPF em CompradorRepository

--- devinhortifrutiapi/src/main/java/tech/devinhouse/devinhortifrutiapi/repository/CompradorRepository.java
+++ devinhortifrutiapi/src/main/java/tech/devinhouse/devinhortifrutiapi/repository/CompradorRepository.java
@@ -1,10 +1,15 @@
 package tech.devinhouse.devinhortifrutiapi.repository;
 
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
 import tech.devinhouse.devinhortifrutiapi.model.Comprador;
 
+import java.util.Optional;
+
 @Repository
-public interface CompradorRepository extends JpaRepository<Comprador, Long> {
+public interface CompradorRepository extends CrudRepository<Comprador, Long>, JpaSpecificationExecutor<Comprador> {
+
+    Optional<Comprador> findByCpf(String cpf);
 
 }
