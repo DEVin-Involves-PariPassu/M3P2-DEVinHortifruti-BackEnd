8c57a9467f1ddfbd6d36ee757dd135e6bee27d62
Lucas Bueno Cesario <lbc92@hotmail.com>
2022-05-27 16:21:31 -0300

    Configura producer de mensagens

--- devinhortifrutiapi/src/main/java/tech/devinhouse/devinhortifrutiapi/DevinhortifrutiapiApplication.java
+++ devinhortifrutiapi/src/main/java/tech/devinhouse/devinhortifrutiapi/DevinhortifrutiapiApplication.java
@@ -1,13 +1,31 @@
 package tech.devinhouse.devinhortifrutiapi;
 
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tech.devinhouse.devinhortifrutiapi.constants.RabbitMQConstants;
+import tech.devinhouse.devinhortifrutiapi.dto.SmsDto;
 
 @SpringBootApplication
+//public class DevinhortifrutiapiApplication implements CommandLineRunner {
 public class DevinhortifrutiapiApplication {
+
+	@Autowired
+	private AmqpTemplate amqpTemplate;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevinhortifrutiapiApplication.class, args);
 	}
 
+//	@Override
+//	public void run(String... args) throws Exception {
+//		System.out.println("Sending message...");
+//		SmsDto sms = new SmsDto();
+//		sms.setDestinatario("+5548991224064");
+//		sms.setMensagem("Teste");
+//		amqpTemplate.convertAndSend(RabbitMQConstants.EXCHANGE, RabbitMQConstants.FILA_SMS, sms);
+//	}
 }
---
+++ devinhortifrutiapi/src/main/java/tech/devinhouse/devinhortifrutiapi/dto/SmsDto.java
@@ -0,0 +1,31 @@
+package tech.devinhouse.devinhortifrutiapi.dto;
+
+public class SmsDto {
+    private String destinatario;
+
+    private String mensagem;
+
+    public SmsDto(String destinatario, String mensagem) {
+        this.destinatario = destinatario;
+        this.mensagem = mensagem;
+    }
+
+    public SmsDto() {
+    }
+
+    public String getDestinatario() {
+        return destinatario;
+    }
+
+    public void setDestinatario(String destinatario) {
+        this.destinatario = destinatario;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+}
