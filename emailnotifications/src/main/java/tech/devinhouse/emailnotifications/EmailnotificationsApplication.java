560a3a1c49866af1521284c5ef1ba566aca01c0c
Lucas Bueno Cesario <lbc92@hotmail.com>
2022-05-27 15:05:16 -0300

    Inicializa microsserviço de notificação

---
+++ emailnotifications/src/main/java/tech/devinhouse/emailnotifications/EmailnotificationsApplication.java
@@ -0,0 +1,13 @@
+package tech.devinhouse.emailnotifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailnotificationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailnotificationsApplication.class, args);
+	}
+
+}
---
+++ emailnotifications/src/test/java/tech/devinhouse/emailnotifications/EmailnotificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.devinhouse.emailnotifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailnotificationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
