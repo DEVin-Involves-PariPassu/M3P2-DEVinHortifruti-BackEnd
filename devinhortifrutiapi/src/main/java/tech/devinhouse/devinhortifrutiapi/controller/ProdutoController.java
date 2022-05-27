88d8a26a57eee3290169108e6616f7794f872e11
Victor Ferrando <victor.ferrando@hotmail.com>
2022-05-26 21:53:36 -0300

    feat: Adiciona Endpoint POST de produto

--- devinhortifrutiapi/src/main/java/tech/devinhouse/devinhortifrutiapi/controller/ProdutoController.java
+++ devinhortifrutiapi/src/main/java/tech/devinhouse/devinhortifrutiapi/controller/ProdutoController.java
@@ -1,9 +1,16 @@
 package tech.devinhouse.devinhortifrutiapi.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tech.devinhouse.devinhortifrutiapi.dto.ProdutoDTO;
 import tech.devinhouse.devinhortifrutiapi.service.ProdutoService;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/produtos")
@@ -11,4 +18,13 @@ public class ProdutoController {
 
     @Autowired
     ProdutoService produtoService;
+
+    @PostMapping
+    public ResponseEntity<Long> postProduto(
+           @Valid @RequestBody ProdutoDTO produtoDTO
+    ){
+        Long produtoId = produtoService.adicionaProduto(produtoDTO);
+        return new ResponseEntity<>(produtoId, HttpStatus.CREATED);
+    }
+
 }
