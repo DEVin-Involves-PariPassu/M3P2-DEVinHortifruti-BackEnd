877cb590023540a008e103a1f23e8a9d7e057463
Douglas Ialamov <douglas.ialamov@gmail.com>
2022-05-29 18:23:55 -0300

    feat: Feature Autenticacao Correções
    src/main/java/tech/devinhouse/devinhortifrutiapi/configuration/AutenticacaoTokenFilter.java
    
      - Corrigido problema da importação OncePerRequestFilter, que era erro no nome e não na importação em si.
      - Corrigido erro para fazer o getAuthorities em usuario.
    
    src/main/java/tech/devinhouse/devinhortifrutiapi/configuration/SecurityConfigurations.java
    
      - No método configure foi retirada a permitAll para outras rotas, mantendo somente público o login.
    
    src/main/java/tech/devinhouse/devinhortifrutiapi/model/Usuario.java
    
      - Implementado o UserDetails e por consequencia todas os métodos get derivados, inclusive o getAuthorities
      que estava em outro local.
    
    src/main/java/tech/devinhouse/devinhortifrutiapi/repository/UsuarioRepository.java
    
      - Retirado o getAuthorities da interface, sugestão de implementação que havia ser feito pelo intellij.
      Isto foi corrigido neste momento.

--- devinhortifrutiapi/src/main/java/tech/devinhouse/devinhortifrutiapi/repository/UsuarioRepository.java
+++ devinhortifrutiapi/src/main/java/tech/devinhouse/devinhortifrutiapi/repository/UsuarioRepository.java
@@ -2,11 +2,9 @@ package tech.devinhouse.devinhortifrutiapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Repository;
 import tech.devinhouse.devinhortifrutiapi.model.Usuario;
 
-import java.util.Collection;
 import java.util.Optional;
 
 
@@ -18,6 +16,4 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long>, JpaSpec
     Optional<Usuario> findUsuarioByNome(String nome);
 
     Optional<Usuario> findUsuarioById(Long usuarioId);
-
-    Collection<? extends GrantedAuthority> getAutoridades();
 }
