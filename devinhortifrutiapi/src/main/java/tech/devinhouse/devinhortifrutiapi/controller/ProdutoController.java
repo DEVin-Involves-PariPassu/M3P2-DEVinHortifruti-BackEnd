747f93dab810ce4d91d84bfb9066fb9f87394a22
Bruno Curcio <braironc@hotmail.com>
2022-06-02 14:05:11 -0300

    feat: cria GET/produto/{id_produto}

--- devinhortifrutiapi/src/main/java/tech/devinhouse/devinhortifrutiapi/controller/ProdutoController.java
+++ devinhortifrutiapi/src/main/java/tech/devinhouse/devinhortifrutiapi/controller/ProdutoController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import tech.devinhouse.devinhortifrutiapi.dto.ProdutoDTO;
 import tech.devinhouse.devinhortifrutiapi.service.ProdutoService;
+import javax.persistence.EntityNotFoundException;
 import javax.validation.*;
 import javax.validation.constraints.NotNull;
 
@@ -15,6 +16,17 @@ public class ProdutoController {
 
     @Autowired
     ProdutoService produtoService;
+
+    @GetMapping(value = "/{id_produto}")
+    public ResponseEntity<?> getProduto(@RequestParam Long id) {
+        try{
+            return ResponseEntity.ok(this.produtoService.getProduto(id));
+        }catch(EntityNotFoundException e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.FORBIDDEN);
+        }
+    }
 
     @PostMapping
     public ResponseEntity<Long> postProduto(
